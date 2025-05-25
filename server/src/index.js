53d763ac2f9f4a72992eb7383e3b1ffc0f4f775a
rohan samanta <rohansamanta45@gmail.com>
2025-05-25 12:11:01 +0530

    setup and database connection

---
+++ server/src/index.js
@@ -0,0 +1,20 @@
+import dotenv from "dotenv";
+import { app } from "./app.js";
+import connectDB from "./db/index.db.js";
+
+dotenv.config();
+
+connectDB()
+  .then(() => {
+    app.on("error", (err) => {
+      console.error("Server error:", err);
+      process.exit(1);
+    });
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on port http://localhost:${process.env.PORT}/`);
+    });
+  })
+  .catch((error) => {
+    console.error("Error starting the server:", error);
+    process.exit(1);
+  });
